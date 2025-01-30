a65c6fac591a8d8765326385550b786409ccf02a
Sarin Patel <sarin.patel7@gmail.com>
2025-01-29 18:14:10 -0800

    Chatbot app
    - Gradio app that takes text or image as input/prompt and uses gemini to process the prompt
    - text or image can be used as an input

---
+++ run.py
@@ -0,0 +1,71 @@
+
+import os
+import PIL.Image
+import gradio as gr
+import base64
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+# Set Google API key 
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+# Define the Model for text and visual
+txt_model = genai.GenerativeModel('gemini-1.5-flash')
+vis_model = genai.GenerativeModel('gemini-1.5-flash')
+
+# Image to Base 64 Converter
+def image_to_base64(image_path):
+    with open(image_path, 'rb') as img:
+        encoded_string = base64.b64encode(img.read())
+    return encoded_string.decode('utf-8')
+
+# Function that takes User Inputs and displays it on ChatUI
+def query_message(history,txt,img):
+    if not img:
+        history += [(txt,None)]
+        return history
+    base64 = image_to_base64(img)
+    data_url = f"data:image/jpeg;base64,{base64}"
+    history += [(f"{txt} ![]({data_url})", None)]
+    return history
+
+# Function that takes User Inputs, generates Response and displays on Chat UI
+def llm_response(history,text,img):
+    if not img:
+        response = txt_model.generate_content(text)
+        history += [(None,response.text)]
+        return history
+
+    else:
+        img = PIL.Image.open(img)
+        response = vis_model.generate_content([text,img])
+        history += [(None,response.text)]
+        return history
+
+# Interface Code
+with gr.Blocks() as app:
+    with gr.Row():
+        image_box = gr.Image(type="filepath")
+    
+        chatbot = gr.Chatbot(
+            scale = 4,
+            height=750
+        )
+    text_box = gr.Textbox(
+            placeholder="Enter text and press enter, or upload an image",
+            container=False,
+        )
+
+    btn = gr.Button("Submit")
+    clicked = btn.click(query_message,
+                        [chatbot,text_box,image_box],
+                        chatbot
+                        ).then(llm_response,
+                                [chatbot,text_box,image_box],
+                                chatbot
+                                )
+
+app.queue()
+app.launch(debug=True)
